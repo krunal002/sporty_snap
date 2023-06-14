09c28b19cf0b7a9bf689d2ef481c893b3aecd52c
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 16:38:00 +0530

    landing page ready with css

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,51 @@
-import "./LandingPage.css"
-import { Link } from "react-router-dom";
+import "./LandingPage.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="landingDiv">
-      <h1 className="heading"><span className="headingSpan">Sporty</span> Snap</h1>
-      <Link to="/home">Home</Link>
+      <div className="leftContainer-landing">
+        <div className="headingDiv">
+          <h1 className="heading">
+            <span className="headingSpan">Sporty</span> Snap
+          </h1>
+          <p className="tagline">
+            <small>Level up your Sportsman Spirit!</small>
+          </p>
+        </div>
+        <div>
+          <p>
+            <small>
+              <span className="largeFont">Click</span> outdoor sports
+            </small>
+          </p>
+          <p>
+            <small>
+              <span className="largeFont">SHARE</span> what you play
+            </small>
+          </p>
+          <p>
+            <small>
+              <span className="largeFont">CONNECT</span> with Sportsmen
+            </small>
+          </p>
+        </div>
+        <div className="landingBtnDiv">
+          <button onClick={() => navigate("/home")} className="landingBtn">
+            <b>Join Now</b>
+          </button>
+          <p className="login-landing">Already have an account?</p>
+        </div>
+      </div>
+      <div className="rightContainer-landing">
+        <img
+          src="https://i.pinimg.com/736x/4a/cd/d8/4acdd87b77d1c9ea547b1ff05614e0de.jpg"
+          alt="landingImage"
+          className="landingImage"
+        />
+      </div>
     </div>
   );
 };
