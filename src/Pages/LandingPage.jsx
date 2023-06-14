b440c7c033fe79aed54e0f17565e02454c1463a6
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 14:26:32 +0530

    Landing page created

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,6 @@
+const LandingPage = () => {
+    return <div>
+        <h1>Landing Page</h1>
+    </div>
+}
+export default LandingPage;
--- src/SportySnap.jsx
+++ src/SportySnap.jsx
@@ -1,6 +1,9 @@
+import LandingPage from "./Pages/LandingPage";
+
 const SportySnap = () => {
     return <div>
         <h1>Sporty Snap!</h1>
+        <LandingPage/>
     </div>
 }
 export default SportySnap;
