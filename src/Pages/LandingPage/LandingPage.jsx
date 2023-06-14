f2a3c11785be5907f92a4e7657c7592ce2ed1369
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 17:01:56 +0530

    updates

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import "./LandingPage.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
   const navigate = useNavigate();
