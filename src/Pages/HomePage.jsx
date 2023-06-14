8e608c681d58a813c8edd059bb371d935d4fa64e
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 14:44:31 +0530

    Profile link added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
     return <div>
         <h1>Home Page</h1>
+        <Link to="/profile"> Profile </Link>
     </div>
 }
 export default Home;
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
-    return <div>
-        <h1>Landing Page</h1>
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <Link to="/home">Home</Link>
     </div>
-}
+  );
+};
 export default LandingPage;
