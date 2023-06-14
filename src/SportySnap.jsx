d513ee65facf83d21c6714c89b5468254c3e2975
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 14:42:50 +0530

    Home link added

--- src/SportySnap.jsx
+++ src/SportySnap.jsx
@@ -2,20 +2,19 @@ import LandingPage from "./Pages/LandingPage";
 import Home from "./Pages/HomePage";
 import Profile from "./Pages/ProfilePage";
 
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 const SportySnap = () => {
-    return <div>
-        <h1>Sporty Snap!</h1>
-        
-       
-        
+  return (
+    <div>
+      <h1>Sporty Snap!</h1>
 
-        <Routes>
-            <Route path="/" element={<LandingPage/>} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
-}
+  );
+};
 export default SportySnap;
