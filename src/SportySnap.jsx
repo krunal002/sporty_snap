557dc367d7697be4cac99a8207a79dbce897a50b
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 14:30:10 +0530

    Profile page created

--- src/SportySnap.jsx
+++ src/SportySnap.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./Pages/LandingPage";
 import Home from "./Pages/HomePage";
+import Profile from "./Pages/ProfilePage";
 
 const SportySnap = () => {
     return <div>
         <h1>Sporty Snap!</h1>
         <LandingPage/>
-        <Home/>
+        <Home />
+        <Profile />
     </div>
 }
 export default SportySnap;
