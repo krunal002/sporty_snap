c4eda7d112ec6dc130ec0798e852cd649f30617a
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-14 14:40:59 +0530

    Routes were created

--- src/SportySnap.jsx
+++ src/SportySnap.jsx
@@ -2,12 +2,20 @@ import LandingPage from "./Pages/LandingPage";
 import Home from "./Pages/HomePage";
 import Profile from "./Pages/ProfilePage";
 
+import { Routes, Route } from "react-router-dom"
+
 const SportySnap = () => {
     return <div>
         <h1>Sporty Snap!</h1>
-        <LandingPage/>
-        <Home />
-        <Profile />
+        
+       
+        
+
+        <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
     </div>
 }
 export default SportySnap;
