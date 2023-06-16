638a95732dc54ee0693c8258e73e0230b1fc0fd0
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-16 13:43:47 +0530

    flex added in home css

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -9,44 +9,52 @@ const Home = () => {
   const { userData } = useContext(UserContext);
 
   return (
-    <div>
+    <div className="home-container">
       <h1>Home Page</h1>
       <button onClick={() => console.log(userData)}>click</button>
 
-      <hr />
-      {userData.map((user) => (
-        <div key={user.id}>
-          <div>
-            <b>
-              {user.firstName} {user.lastName}
-            </b>
-          </div>
-          <div>@{user.username}</div>
-        </div>
-      ))}
-      <hr />
+      <div className="primary-container">
+      <div className="link-container-div">
+        <Link to="/profile"> Profile </Link>
+        <Link to="/login"> Login </Link>
+      </div>
 
-      <hr />
-      {postData.map((post) => (
-        <div key={post.id} className="post-container">
-          <div className="userDetails">
-            <div className="userImg">
-              <img src={post.userImage} alt="userImage" className="userImage"/>
-            </div>
-            <div className="userInfo">
-              <h4>@{post.username}</h4>
-            </div>
-            
-          </div>
-          
-          <img src={post.img} alt="sportyImage" className="postImage" />
-          <p>content : {post.content}</p>
-        </div>
-      ))}
-      <hr />
+      <div className="post-container-div">
+        {postData.map((post) => (
+          <div key={post.id} className="post-container">
+            <div className="userDetails">
+              <div className="userImg">
+                <img
+                  src={post.userImage}
+                  alt="userImage"
+                  className="userImage"
+                />
+              </div>
 
-      <Link to="/profile"> Profile </Link>
-      <Link to="/login"> Login </Link>
+              <div className="userInfo">
+                <h4>@{post.username}</h4>
+              </div>
+            </div>
+
+            <img src={post.img} alt="sportyImage" className="postImage" />
+            <p>content : {post.content}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="user-container-div">
+        {userData.map((user) => (
+          <div key={user.id} className="user-container">
+            <p>
+              <b>
+                {user.firstName} {user.lastName}
+              </b>
+            </p>
+            <p>@{user.username}</p>
+          </div>
+        ))}
+      </div>
+      </div>
     </div>
   );
 };
