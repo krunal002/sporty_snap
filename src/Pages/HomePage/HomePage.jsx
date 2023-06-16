d249003a026cd2d319b9f13b6bc3dfe8586d6d9e
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-16 16:29:12 +0530

    update

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -15,13 +15,28 @@ const Home = () => {
 
       <div className="primary-container">
         <div className="link-container-div">
-          <Link to="/profile" className="home-links"> Profile </Link>
-          <Link to="/login" className="home-links"> Login </Link>
+          <div className="link-container container-format">
+            <Link to="/home" className="home-links">
+              <i class="fa fa-home" aria-hidden="true"></i> Home
+            </Link>
+            <Link to="/#" className="home-links">
+              <i class="fa fa-suitcase" aria-hidden="true"></i> Explore
+            </Link>
+            <Link to="/profile" className="home-links">
+              <i class="fa fa-bookmark" aria-hidden="true"></i> Bookmark
+            </Link>
+            <Link to="/profile" className="home-links">
+              <i class="fa fa-user-circle-o" aria-hidden="true"></i> Profile
+            </Link>
+            <Link to="/login" className="home-links">
+              <i class="fa fa-sign-in" aria-hidden="true"></i> Login
+            </Link>
+          </div>
         </div>
 
         <div className="post-container-div">
           {postData.map((post) => (
-            <div key={post.id} className="post-container">
+            <div key={post.id} className="post-container container-format">
               <div className="userDetails">
                 <div className="userImg">
                   <img
@@ -43,16 +58,32 @@ const Home = () => {
         </div>
 
         <div className="user-container-div">
-          {userData.map((user) => (
-            <div key={user.id} className="user-container">
-              <p>
-                <b>
-                  {user.firstName} {user.lastName}
-                </b>
-              </p>
-              <p>@{user.username}</p>
-            </div>
-          ))}
+          <div className="user-container container-format">
+            {userData.map((user) => (
+              <div key={user.id} className="userDetails userDetails-margin">
+                <div>
+                  <img
+                    src={user.userImage}
+                    alt="userImage"
+                    className="userImage"
+                  />
+                </div>
+
+                <div className="userInfo">
+                  <p>
+                    <b>
+                      {user.firstName} {user.lastName}
+                    </b>
+                  </p>
+                  <p>@{user.username}</p>
+                </div>
+
+                <div className="follow-user">
+                  <b>Follow</b>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
