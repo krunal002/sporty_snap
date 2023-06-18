03676972c1c468fb9d0026fe85eafedd4dccec0f
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-18 15:57:32 +0530

    like functioning added

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,90 +1,87 @@
 import "./HomePage.css";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+
+import { useContext, useState } from "react";
 import { PostContext } from "../../SportySnap";
-import { UserContext } from "../../Context/userContext";
+import Links from "../../Components/Links";
+import Users from "../../Components/Users";
 
 const Home = () => {
-  const { postData } = useContext(PostContext);
-  const { userData } = useContext(UserContext);
+  const { postData, likeIncreament, likeDecreament } = useContext(PostContext);
+  const [likeValue, setLikeValue] = useState(false);
 
+  const likeHandler = (post) => {
+    // console.log(postData.likes.likeCount)
+    setLikeValue(!likeValue);
+    likeValue ? likeDecreament(post) : likeIncreament(post);
+  };
   return (
     <div className="home-container">
       <h1>Home Page</h1>
-      <button onClick={() => console.log(userData)}>click</button>
 
       <div className="primary-container">
-        <div className="link-container-div">
-          <div className="link-container container-format">
-            <Link to="/home" className="home-links">
-              <i class="fa fa-home" aria-hidden="true"></i> Home
-            </Link>
-            <Link to="/#" className="home-links">
-              <i class="fa fa-suitcase" aria-hidden="true"></i> Explore
-            </Link>
-            <Link to="/profile" className="home-links">
-              <i class="fa fa-bookmark" aria-hidden="true"></i> Bookmark
-            </Link>
-            <Link to="/profile" className="home-links">
-              <i class="fa fa-user-circle-o" aria-hidden="true"></i> Profile
-            </Link>
-            <Link to="/login" className="home-links">
-              <i class="fa fa-sign-in" aria-hidden="true"></i> Login
-            </Link>
-          </div>
-        </div>
+        {/* Linnks */}
+        <Links />
 
+        {/* post */}
         <div className="post-container-div">
-          {postData.map((post) => (
-            <div key={post.id} className="post-container container-format">
-              <div className="userDetails">
-                <div className="userImg">
-                  <img
-                    src={post.userImage}
-                    alt="userImage"
-                    className="userImage"
-                  />
+          {postData.map((post) => {
+            return (
+              <div key={post.id} className="post-container container-format">
+                <div className="userDetails">
+                  <div className="userImg">
+                    <img
+                      src={post.userImage}
+                      alt="userImage"
+                      className="userImage"
+                    />
+                  </div>
+
+                  <div className="userInfo">
+                    <h4>@{post.username}</h4>
+                  </div>
                 </div>
 
-                <div className="userInfo">
-                  <h4>@{post.username}</h4>
+                <div className="postImage-container">
+                  <img src={post.img} alt="sportyImage" className="postImage" />
+                </div>
+                <p>content : {post.content}</p>
+                <div className="opertionalBtn">
+                  <div className="leftBtn">
+                    <div
+                      className="like sign"
+                      onClick={() => likeHandler(post)}
+                    >
+                      {likeValue ? (
+                        <i class="fa fa-heart" aria-hidden="true">
+                          {" "}
+                        </i>
+                      ) : (
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                      )}{" "}
+                      {post.likes.likeCount}
+                    </div>
+                    <div className="comment sign">
+                      <i class="fa fa-comment-o" aria-hidden="true">
+                        {" "}
+                        {post.comments.length}
+                      </i>
+                    </div>
+                    <div className="share sign">
+                      <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                    </div>
+                  </div>
+
+                  <div className="bookmark sign">
+                    <i class="fa fa-bookmark-o" aria-hidden="true"></i>
+                  </div>
                 </div>
               </div>
-
-              <img src={post.img} alt="sportyImage" className="postImage" />
-              <p>content : {post.content}</p>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
-        <div className="user-container-div">
-          <div className="user-container container-format">
-            {userData.map((user) => (
-              <div key={user.id} className="userDetails userDetails-margin">
-                <div>
-                  <img
-                    src={user.userImage}
-                    alt="userImage"
-                    className="userImage"
-                  />
-                </div>
-
-                <div className="userInfo">
-                  <p>
-                    <b>
-                      {user.firstName} {user.lastName}
-                    </b>
-                  </p>
-                  <p>@{user.username}</p>
-                </div>
-
-                <div className="follow-user">
-                  <b>Follow</b>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        {/* user */}
+        <Users />
       </div>
     </div>
   );
