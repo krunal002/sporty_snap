7603ced156e134c7505248868a103edc793d86e1
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-11-10 16:11:54 +0530

    scrollbar added on home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         </div>
 
         {/* post */}
-        <div>
+        <div className="post-wrapper">
           <CreatePost />
           <PostCard item={homeData} />
         </div>
