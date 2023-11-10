99575c3a83a73d98783583cb9714e1704c732f24
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-11-10 16:47:40 +0530

    bookmark page edited

--- src/Pages/BookmarkPage/Bookmark.jsx
+++ src/Pages/BookmarkPage/Bookmark.jsx
@@ -1,13 +1,11 @@
 import "./Bookmark.css";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { BookmarkContext, PostContext } from "../../SportySnap";
 import UserCard from "../../Cards/UserCard";
 import LinkCard from "../../Cards/LinkCard";
 import PostCard from "../../Cards/PostCard";
 
 const Bookmark = () => {
-  const currUser = JSON.parse(localStorage.getItem("user"));
   const { postData } = useContext(PostContext);
 
   const { bookmarkData } = useContext(BookmarkContext);
@@ -17,37 +15,30 @@ const Bookmark = () => {
   return (
     <div className="bookmark-container">
       <div className="home-container">
-        <div className="nav-container">
-          <div className="nav-heading">
-            <h1>
-              <i class="fa fa-bookmark" aria-hidden="true"></i> Bookmarks
-            </h1>
-          </div>
-
-          <div>
-            <b className="bookmark-links">Hi, {currUser.firstName}</b>
-            <Link to="/login" className="bookmark-links">
-              <i class="fa fa-sign-in" aria-hidden="true"></i>
-            </Link>
-          </div>
-        </div>
-
         <div className="primary-container">
           {/* links */}
-          <LinkCard />
+          <div className="link-wrapper">
+            <LinkCard />
+          </div>
 
           {/* post */}
-          {!newBD.length ? (
-            <div className="emptyBookmark">
-              <h3>Nothing to show!</h3>
-              <p>Please add some post to Bookmark</p>
-            </div>
-          ) : (
-            <PostCard item={newBD}/>
-          )}
+          <div className="post-wrapper">
+            {!newBD.length ? (
+              <div className="emptyBookmark">
+                <h3>Nothing to show!</h3>
+                <p>Please add some post to Bookmark</p>
+              </div>
+            ) : (
+              <div>
+                <PostCard item={newBD} />
+              </div>
+            )}
+          </div>
 
           {/* user */}
-          <UserCard />
+          <div className="users-wrapper">
+            <UserCard />
+          </div>
         </div>
       </div>
     </div>
